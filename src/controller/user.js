b14d2a482f0a6eaf4f10311e6e709d86c7de35f6
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-03 17:11:40 -0500

    add \n at file end

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const User = require('../model/User');
 
-router.get('/', (req, res) => {
-	res.send('hello world');
+router.get('/', async (req, res) => {
+	const users = await User.find();
+	res.send(users);
 });
 
 module.exports = router;
